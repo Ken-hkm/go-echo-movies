3daf87c9041d7db6de2e74a5e19630828bd70fc2
kenneth.hakim <kenneth.hakim@sg-edts.com>
2024-11-14 16:31:04 +0700

    clean up codes

--- internal/handler/movie_controller.go
+++ internal/handler/movie_controller.go
@@ -16,19 +16,11 @@ func NewMovieController(movieService *service.MovieService) *MovieController {
 	return &MovieController{movieService: movieService}
 }
 
-// GetMovies is a handler that returns a list of movies
-//func (mc *MovieController) GetMovies(c echo.Context) error {
-//	movies := mc.movieService.GetMovies()
-//	return c.JSON(http.StatusOK, movies)
-//}
-
 // GetMovies retrieves all movies
 func (h *MovieController) GetMovies(c echo.Context) error {
-
 	movies, err := h.movieService.GetMovies()
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Failed to retrieve movies"})
 	}
-
 	return c.JSON(http.StatusOK, movies)
 }
